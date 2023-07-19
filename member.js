d1964f50116e582fcc5aae084f01fb2e186de225
Rodrigo Huarachi <69877961+rodrihuarachi@users.noreply.github.com>
2023-07-19 03:10:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberDiv = document.getElementById("memberDiv");
+    var skillsDiv = document.getElementById("skillsDiv");
+    var projectsDiv = document.getElementById("projectsDiv");
+    var contactDiv = document.getElementById("contactDiv");
+    var aboutDiv = document.getElementById("aboutDiv");
+    var homeDiv = document.getElementById("homeDiv");
+    member.style.color = "white";
+    skills.style.color = "black";
+    projects.style.color = "black";
+    contact.style.color = "black";
+    about.style.color = "black";
+    home.style.color = "black";
+    memberDiv.style.display = "block";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+    aboutDiv.style.display = "none";
+    homeDiv.style.display = "none";
+}
